1a588acd8fcad25008fb947cf79c923a588e2e9d
abbypan <abbypan@gmail.com>
2013-09-23 20:50:41 +0800

    hist bar facets

---
+++ hist_bar-facets-ggplot2.r
@@ -0,0 +1,8 @@
+require(reshape2)
+library(plyr)
+library(ggplot2)
+
+raw_data <- read.table('hist_bar.csv',header=TRUE, sep=',')
+p <- qplot(calc_a,data=raw_data,facets = . ~ hkey + ukey + pkey,
+           binwidth=3,color=pkey)
+ggsave(p, filename="hist_bar-facets-ggplot2.png" , width = 8)
