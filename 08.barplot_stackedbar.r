26957b1e5e2030a4472d6bcf43a89728aae5a02b
Abby Pan <abbypan@gmail.com>
2015-06-10 17:28:44 +0800

    encoding

--- 08.barplot_stackedbar.r
+++ 08.barplot_stackedbar.r
@@ -1,3 +1,5 @@
+#注意文件编码必须为本地系统编码
+
 data_a <- c(
             89,
             3,
@@ -12,15 +14,19 @@ data_b <- c(
 
 data_m <- matrix(c(data_a,data_b), 3, 2)
 
+png("08.barplot_stackedbar.png")
+
 barplot(
         data_m, 
-        angle=c(45,90,135), 
-        density=c(10,10,10), 
+        angle=c(45,90,135),  #填充的斜线角度
+        density=c(10,10,10),  #填充的斜线密度
 
         main="测试",
         names.arg=c('OBJ_A','OBJ_B'),
 
-        xlim=c(0,3),
+        xlim=c(0,3),  #x轴宽度
         legend.text = c("优","良","差"), 
-        args.legend = list(x = 3, y = 50)
+        args.legend = list(x = 3, y = 50)  #图例位置
         ) 
+
+dev.off()
